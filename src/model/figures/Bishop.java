30f61c4c404710501c2a0fc3580e5706c96416fc
Kacper Roszczyna <kacper.ros94@gmail.com>
2016-04-30 00:51:05 +0200

    Bishop possible fields for movement added

--- src/model/figures/Bishop.java
+++ src/model/figures/Bishop.java
@@ -1,5 +1,6 @@
 package model.figures;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import exceptions.InvalidMoveException;
@@ -9,7 +10,7 @@ import model.Model;
 
 /**
  * Goniec
- * */
+ */
 public class Bishop extends ChessPiece {
 
 	public Bishop(Model.Color color, Board board) {
@@ -17,7 +18,7 @@ public class Bishop extends ChessPiece {
 	}
 
 	@Override
-	public void movePiece(Field pieceField, Field targetField) throws InvalidMoveException{
+	public void movePiece(Field pieceField, Field targetField) throws InvalidMoveException {
 		if (!isMovePossible(pieceField, targetField))
 			throw new InvalidMoveException();
 		else {
@@ -30,19 +31,18 @@ public class Bishop extends ChessPiece {
 	public boolean isMovePossible(Field pieceField, Field targetField) {
 		int deltaX = targetField.getFieldCoordintes().x - pieceField.getFieldCoordintes().x;
 		int deltaY = targetField.getFieldCoordintes().y - pieceField.getFieldCoordintes().y;
-		if(Math.abs(deltaX) != Math.abs(deltaY))
+		if (Math.abs(deltaX) != Math.abs(deltaY))
 			return false;
-		int dx = deltaX/Math.abs(deltaX);
-		int dy = deltaY/Math.abs(deltaY);
+		int dx = deltaX / Math.abs(deltaX);
+		int dy = deltaY / Math.abs(deltaY);
 		int startX = pieceField.getFieldCoordintes().x;
 		int startY = pieceField.getFieldCoordintes().y;
 		Field nextField;
-		for(int i = 1; i<= Math.abs(deltaX); i++){
-			nextField = board.getField(startX + i*dx, startY + i*dy);
-			if(nextField.getChessPiece() != null 
-					&& (i != Math.abs(deltaX) 
-						|| nextField.getChessPiece().getColor() == this.getColor())){
-					return false;
+		for (int i = 1; i <= Math.abs(deltaX); i++) {
+			nextField = board.getField(startX + i * dx, startY + i * dy);
+			if (nextField.getChessPiece() != null
+					&& (i != Math.abs(deltaX) || nextField.getChessPiece().getColor() == this.getColor())) {
+				return false;
 			}
 		}
 		return true;
@@ -50,8 +50,31 @@ public class Bishop extends ChessPiece {
 
 	@Override
 	public List<Field> getPossibleMoves(Field pieceField) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Field> possibleFields = new ArrayList<>();
+		int dx[] = { 1, 1, -1, -1 };
+		int dy[] = { 1, -1, 1, -1 };
+		for (int i = 0; i < 4; i++)
+			possibleFields.addAll(getPossibleFieldsInDirection(pieceField, dx[i], dy[i]));
+		return possibleFields;
+	}
+
+	private List<Field> getPossibleFieldsInDirection(Field pieceField, int dx, int dy) {
+		List<Field> result = new ArrayList<>();
+		int x = pieceField.getFieldCoordintes().x + dx;
+		int y = pieceField.getFieldCoordintes().y + dy;
+		Field nextField;
+		while (x <= 7 && y <= 7 && x >= 0 && y >= 0) {
+			nextField = board.getField(x, y);
+			if (nextField.getChessPiece() != null) {
+				if (!this.getColor().equals(nextField.getChessPiece().getColor()))
+					result.add(nextField);
+				break;
+			}
+			result.add(nextField);
+			x = x + dx;
+			y = y + dy;
+		}
+		return result;
 	}
 
 }
--- test/model/figures/BishopTest.java
+++ test/model/figures/BishopTest.java
@@ -2,6 +2,8 @@ package model.figures;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,115 +14,138 @@ import model.Model;
 
 public class BishopTest {
 	private Board board;
-	
+
 	@Before
-	public void setupBoard(){
+	public void setupBoard() {
 		board = new Board();
 		board.emptyBoard();
 		Field testField = board.getField(3, 3);
 		testField.setChessPiece(new Bishop(Model.Color.black, board));
 	}
-	
+
 	@Test
-	public void testMoveUpLeft() throws Exception{
+	public void testMoveUpLeft() throws Exception {
 		Field startField = board.getField(3, 3);
 		ChessPiece bishop = startField.getChessPiece();
 		bishop.movePiece(startField, board.getField(1, 5));
-		
+
 		Field testField = board.getField(1, 5);
 		assertTrue(testField.getChessPiece().getName().equals(bishop.getName()));
 		assertTrue(testField.getChessPiece().getColor().equals(bishop.getColor()));
 	}
-	
+
 	@Test
-	public void testMoveUpRight() throws Exception{
+	public void testMoveUpRight() throws Exception {
 		Field startField = board.getField(3, 3);
 		ChessPiece bishop = startField.getChessPiece();
-		bishop.movePiece(startField, board.getField(6,6));
-		
-		Field testField = board.getField(6,6);
+		bishop.movePiece(startField, board.getField(6, 6));
+
+		Field testField = board.getField(6, 6);
 		assertTrue(testField.getChessPiece().getName().equals(bishop.getName()));
 		assertTrue(testField.getChessPiece().getColor().equals(bishop.getColor()));
 	}
-	
+
 	@Test
-	public void testMoveDownLeft() throws Exception{
+	public void testMoveDownLeft() throws Exception {
 		Field startField = board.getField(3, 3);
 		ChessPiece bishop = startField.getChessPiece();
 		bishop.movePiece(startField, board.getField(0, 0));
-		
+
 		Field testField = board.getField(0, 0);
 		assertTrue(testField.getChessPiece().getName().equals(bishop.getName()));
 		assertTrue(testField.getChessPiece().getColor().equals(bishop.getColor()));
 	}
-	
+
 	@Test
-	public void testMoveDownRight() throws Exception{
+	public void testMoveDownRight() throws Exception {
 		Field startField = board.getField(3, 3);
 		ChessPiece bishop = startField.getChessPiece();
 		bishop.movePiece(startField, board.getField(6, 0));
-		
+
 		Field testField = board.getField(6, 0);
 		assertTrue(testField.getChessPiece().getName().equals(bishop.getName()));
 		assertTrue(testField.getChessPiece().getColor().equals(bishop.getColor()));
 	}
-	
-	@Test (expected = InvalidMoveException.class)
-	public void testMoveUpLeft_Obstacle() throws Exception{
+
+	@Test(expected = InvalidMoveException.class)
+	public void testMoveUpLeft_Obstacle() throws Exception {
 		Field startField = board.getField(3, 3);
-		Bishop bishop = (Bishop)startField.getChessPiece();
+		Bishop bishop = (Bishop) startField.getChessPiece();
 		board.getField(2, 4).setChessPiece(new Bishop(Model.Color.black, board));
 		bishop.movePiece(startField, board.getField(1, 5));
 	}
-	
+
 	@Test(expected = InvalidMoveException.class)
-	public void testMoveUpRight_Obstacle() throws Exception{
+	public void testMoveUpRight_Obstacle() throws Exception {
 		Field startField = board.getField(3, 3);
 		ChessPiece bishop = startField.getChessPiece();
 		board.getField(4, 4).setChessPiece(new Bishop(Model.Color.black, board));
-		bishop.movePiece(startField, board.getField(6,6));
+		bishop.movePiece(startField, board.getField(6, 6));
 	}
-	
+
 	@Test(expected = InvalidMoveException.class)
-	public void testMoveDownLeft_Obstacle() throws Exception{
+	public void testMoveDownLeft_Obstacle() throws Exception {
 		Field startField = board.getField(3, 3);
 		ChessPiece bishop = startField.getChessPiece();
 		board.getField(1, 1).setChessPiece(new Bishop(Model.Color.black, board));
-		bishop.movePiece( startField, board.getField(0, 0));
+		bishop.movePiece(startField, board.getField(0, 0));
 	}
-	
+
 	@Test(expected = InvalidMoveException.class)
-	public void testMoveDownRight_Obstacle() throws Exception{
+	public void testMoveDownRight_Obstacle() throws Exception {
 		Field startField = board.getField(3, 3);
 		ChessPiece bishop = startField.getChessPiece();
-		board.getField(5,1).setChessPiece(new Bishop(Model.Color.black, board));
+		board.getField(5, 1).setChessPiece(new Bishop(Model.Color.black, board));
 		bishop.movePiece(startField, board.getField(6, 0));
 	}
-	
+
 	@Test(expected = InvalidMoveException.class)
-	public void testInvalidMove() throws Exception{
+	public void testInvalidMove() throws Exception {
 		Field startField = board.getField(3, 3);
 		ChessPiece bishop = startField.getChessPiece();
 		bishop.movePiece(startField, board.getField(3, 0));
 	}
-	
+
 	@Test(expected = InvalidMoveException.class)
-	public void testKill() throws Exception{
+	public void testKill() throws Exception {
 		Field startField = board.getField(3, 3);
 		ChessPiece bishop = startField.getChessPiece();
 		board.getField(6, 6).setChessPiece(new Rook(Model.Color.black, board));
-		bishop.movePiece(startField, board.getField(6,6));
+		bishop.movePiece(startField, board.getField(6, 6));
 	}
-	
+
 	@Test
-	public void testKillValid() throws Exception{
+	public void testKillValid() throws Exception {
 		Field startField = board.getField(3, 3);
 		ChessPiece bishop = startField.getChessPiece();
 		board.getField(6, 6).setChessPiece(new Rook(Model.Color.white, board));
-		bishop.movePiece(startField, board.getField(6,6));
-		
+		bishop.movePiece(startField, board.getField(6, 6));
+
 		Field testField = board.getField(6, 6);
 		assertTrue(testField.getChessPiece().getName().equals(bishop.getName()));
 		assertTrue(testField.getChessPiece().getColor().equals(bishop.getColor()));
+	}
+
+	@Test
+	public void possibleMoves() throws Exception {
+		Field startField = board.getField(3, 3);
+		ChessPiece bishop = startField.getChessPiece();
+		board.getField(6, 6).setChessPiece(new Bishop(Model.Color.white, board));
+		board.getField(0, 0).setChessPiece(new Bishop(Model.Color.black, board));
+		
+		List<Field> fields = bishop.getPossibleMoves(startField);
+		assertTrue(fields.size() == 11);
+	}
+	
+	@Test
+	public void possibleMovesFromCorner() throws Exception {
+		Field startField = board.getField(3, 3);
+		startField.removeChessPiece();
+		board.getField(6, 6).setChessPiece(new Bishop(Model.Color.white, board));
+		board.getField(0, 0).setChessPiece(new Bishop(Model.Color.black, board));
+		ChessPiece bishop = board.getField(0, 0).getChessPiece();
+		
+		List<Field> fields = bishop.getPossibleMoves(board.getField(0, 0));
+		assertTrue(fields.size() == 6);
 	}
 }
